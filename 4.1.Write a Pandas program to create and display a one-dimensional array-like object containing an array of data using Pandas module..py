6abe88d6de59017111db6255d5e70e71800e67f8
Sri_Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2025-11-06 00:03:26 +0530

    Update 4.1.Write a Pandas program to create and display a one-dimensional array-like object containing an array of data using Pandas module..py

--- 4.1.Write a Pandas program to create and display a one-dimensional array-like object containing an array of data using Pandas module..py
+++ 4.1.Write a Pandas program to create and display a one-dimensional array-like object containing an array of data using Pandas module..py
@@ -3,3 +3,10 @@ import pandas as pd
 data = [18,45,7,33,17,333]
 series = pd.Series(data)
 print(series)
+# Write a Pandas program to convert a Pandas Series to Python list and its type
+import pandas as pd
+data = [18,97,38,61,6,15]
+series = pd.Series(data)
+list_data = series.tolist()
+print(list_data)
+print(type(list_data))
